9ce2d4e9c0e5be698189d4e67f647a57aa155ff5
김영서 <oseo0202@gim-yeongseoui-MacBookPro.local>
2022-05-22 21:07:08 +0900

    [feat] mainPage image-grid layout

--- src/main/webapp/react/mju/src/Routes/Main.tsx
+++ src/main/webapp/react/mju/src/Routes/Main.tsx
@@ -1,10 +1,76 @@
-function Main(){
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { getItemData } from '../api';
+
+interface MainProps{
+    state: string;
+}
+
+const MainWrap = styled.div`
+display: flex;
+justify-content: center;
+section{
+    width: 1000px;
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    gap: 1rem;
+    justify-items: center;
+}
+`;
+const ItemCard = styled.div<MainProps>`
+background-color: ${(props)=> props.state === "판매 완료" && "rgba(1, 1, 1, 0.7)"};
+border: 1px solid black;
+
+.itemCard_itemInfo{
+    padding-left: 5px;
+}
+
+.itemCard_itemTitle{
+    margin: 5px 0;
+    display: block;
+    font-size: 18px;
+}
+
+.itemCard_itemState{
+    border: 1px solid gray;
+    color: ${(props)=> props.state === "판매 완료" && "white"};
+    width: 70px;
+    text-align: center;
+}
+
+.itemCard_itemImg{
+    opacity: ${(props)=> props.state === "판매 완료" && "0.3"};
+}
+`;
+
+function Main() {
+    const [itemData, setItemData] = useState<any[]>([]);
+
+    useEffect(() => {
+        (async () => {
+            const data = await getItemData();
+            setItemData(data);
+        })();
+    }, []);
+
     return (
-        <>
-           <div>
-               Main
-            </div> 
-        </>
+        <MainWrap>
+            <section>
+                
+            {  itemData && itemData.map((data: any) =>
+             <ItemCard state={data.state}>
+             <img className='itemCard_itemImg' width="200" height="250" src={data.picture} alt={data.title} />
+             <div className='itemCard_itemInfo' key={data.id}>
+                 <span className='itemCard_itemTitle'>{data.title}</span>
+                 <span className='itemCard_itemPrice'>{data.price}원</span>
+                 <div className='itemCard_itemState'>{data.state}</div>
+                 {/* <span className='itemCard_itemDate'>{data.date}</span> */}
+             </div>
+         </ItemCard>)
+
+            }
+            </section>
+        </MainWrap>
     );
 }
 
